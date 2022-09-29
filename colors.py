54bb24d0dac8c18d64ac8f27dc368b36076004b3
giulio <giulpiomilani@gmail.com>
2022-09-29 10:41:45 +0000

    new color

--- colors.py
+++ colors.py
@@ -3,19 +3,22 @@ import busio
 import time
 
 i2c = busio.I2C(board.I2C2_SCL, board.I2C2_SDA)
-
-for n in range(254):
-    try:
-        i2c.writeto(0x4, bytes([n,n,n,n]), stop=True)
-        time.sleep(0.04)
-    except:
-        n = n-1
+for i in range(3):	
+	for n in range(50):
+    		try:
+        		i2c.writeto(0x4, bytes([n,n,n,n]), stop=True)
+        		time.sleep(0.01)
+    		except:
+	        	n = n-1
+      	 # print("error")
         
-        time.sleep(0.5)
-for n in range(150):
-    try:
-        i2c.writeto(0x4, bytes([n,254-n,254-n,254-n]), stop=True)
-        time.sleep(0.04)
-    except:
-        n = n-1
+	time.sleep(0.3)
+
+	for n in range(50):
+    		try:
+        		i2c.writeto(0x4, bytes([49-n,49-n,49-n,49-n]), stop=False)
+        		time.sleep(0.01+i/100)
+    		except:
+        		n = n-1
+        		#print("error")
 

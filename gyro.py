c326fda18dd4b9486bda77c1ae147a1e92e90c07
giulpio <giulpiomilani@gmail.com>
2022-09-29 17:35:15 +0200

    add how_to add gyro.py

--- gyro.py
+++ gyro.py
@@ -4,13 +4,20 @@ import board
 import busio
 import adafruit_mpu6050
 
-i2c = busio.I2C(board.I2C2_SCL, board.I2C2_SDA)
+i2c1 = busio.I2C(board.I2C1_SCL, board.I2C1_SDA)
+i2c2 = busio.I2C(board.I2C2_SCL, board.I2C2_SDA)
 
-mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu = adafruit_mpu6050.MPU6050(i2c1)
+
+i2c2.writeto(0x4, bytes([0,255,0,0]), stop=True)
 
 while True:
+    
+    
+    """
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
     print("Temperature: %.2f C" % mpu.temperature)
     print("")
     time.sleep(1)
+    """

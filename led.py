96aa664094d4beb5044ed76cc6f12a0e2a9f9e1d
giulpio <giulpiomilani@gmail.com>
2022-09-30 14:59:25 +0200

    initial library

--- led.py
+++ led.py
@@ -2,8 +2,20 @@ from i2c import i2c2
 import time
 
 def led(r=0, g=0, b=0):
-    try:
-        i2c2.writeto(0x4, bytes([r,r,g,b]), stop=True)
-    except:
-        pass
+    success=False
+    while not success:
+        try:
+            i2c2.writeto(0x4, bytes([r,r,g,b]), stop=True)
+            success = True
+        except:
+            print("suca")
+            pass
 
+
+def led_fade(t):
+    for i in range(255):
+...     b=i-125
+...     if b<0:
+...             b=-b
+...     led(i, 255-i, b)
+...     time.sleep(t)

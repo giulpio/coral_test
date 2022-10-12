076734c88567f342df42ae24b29fc3ea486223a9
giulpio <giulpiomilani@gmail.com>
2022-10-12 15:21:51 +0200

    gyro rev

--- gyro.py
+++ gyro.py
@@ -7,9 +7,13 @@ import adafruit_mpu6050
 mpu = adafruit_mpu6050.MPU6050(i2c1)
 
 while True:
-
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
-    print("Temperature: %.2f C" % mpu.temperature)
-    print("")
+    try:
+        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+        print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+        print("Temperature: %.2f C" % mpu.temperature)
+        print("")
+    except:
+        print("error")
+        
     time.sleep(1)
+    

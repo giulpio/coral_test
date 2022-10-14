499fb5a007a74fc9d6a7c0c56814ede5db6e43bf
giulpio <giulpiomilani@gmail.com>
2022-10-14 13:11:54 +0200

    buttons_test

--- button_test.py
+++ button_test.py
@@ -2,7 +2,40 @@ import board
 import digitalio
 import time
 
-button = digitalio.DigitalInOut(board.GPIO37)  # pin 36
+def get_foo(someobject, foostring):
+    return getattr(someobject,foostring)
+
+
+
+
+while True:
+    for i in [12,22]:
+        l='D'
+        l+=str(i)
+        #print(l)
+        try:
+            button = digitalio.DigitalInOut(get_foo(board, l))
+            
+            b1=button.value
+            time.sleep(0.1)
+            final = b1 and button.value
+
+            print(l + ': ' + str(final))  
+            #print(button.direction)
+            button.deinit()
+        except:
+                pass
+    time.sleep(0.4)
+
+
+
+
+
+
+
+
+"""
+button = digitalio.DigitalInOut(board.PWM_B)  # pin 36
 button.direction = digitalio.Direction.INPUT
 
 value = button.value
@@ -19,3 +52,4 @@ try:
 
 finally:
     button.deinit()
+    """

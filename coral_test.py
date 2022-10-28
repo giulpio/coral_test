99f5428d7b9535c25b386f5e2abc26d6d1691b49
giulpio <giulpiomilani@gmail.com>
2022-10-28 17:36:08 +0200

    coral_test

--- coral_test.py
+++ coral_test.py
@@ -17,22 +17,25 @@ try:
     while True:
         if read(switch):
             print("read_switch")
-            time.sleep(1)
+            #time.sleep(1)
             #state+=1
             #if state > maxstate:
-             #   state = 0
+            #    state = 0
+            led(0,0,0,255)
+            print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.mpu.acceleration))
         if state == 0:
             if read(button1):
                 led(255,0,0)
             elif read(button2):
-                led(0,255,255)
+                led(0,255,0)
             else:
                 led(255,255,255)
         elif state == 1:
             led(0,0,0,255)
         else:
             led(0,0,0)
-        time.sleep(0.2)
+        #time.sleep(0.2)
+
 
 except:
     print("error")

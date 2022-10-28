b9ffb19b551fb85fbc925a2821d04449f16cbe0f
giulpio <giulpiomilani@gmail.com>
2022-10-28 17:04:55 +0200

    coral_test

--- coral_test.py
+++ coral_test.py
@@ -16,6 +16,7 @@ maxstate = 2
 try:
     while True:
         if read(switch):
+            time.sleep(1)
             state+=1
             if state > maxstate:
                 state = 0
@@ -30,6 +31,7 @@ try:
             led(0,0,0,255)
         else:
             led(0,0,0)
+        time.sleep(0.2)
 
 #shutdown.shutdown()
 

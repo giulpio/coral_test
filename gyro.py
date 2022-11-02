acd4f8cbef1b66fa767a8a679d6c52be58c20ae6
giulpio <giulpiomilani@gmail.com>
2022-11-02 11:24:04 +0100

    log gyro delta functions

--- gyro.py
+++ gyro.py
@@ -9,13 +9,15 @@ mpu = adafruit_mpu6050.MPU6050(i2c1)
 old_g= (0,0,0)
 old_a=(0,0,0)
 
+max_g =(0,0,0)
+max_a=(0,0,0)
 
 def delta_acceleration():
     global old_a
-    print("old: %.3f, %.3f, %.3f" %  old_a)
-    print("now: %.3f, %.3f, %.3f" %  mpu.acceleration)
+    #print("old: %.3f, %.3f, %.3f" %  old_a)
+    #print("now: %.3f, %.3f, %.3f" %  mpu.acceleration)
     res = tuple(map(lambda i, j: i - j, mpu.acceleration, old_a))
-    print("res: %.3f, %.3f, %.3f" % res)
+    #print("res: %.3f, %.3f, %.3f" % res)
     old_a = mpu.acceleration
     return res
 
@@ -24,6 +26,8 @@ def delta_gyro():
     res = tuple(map(lambda i, j: i - j, mpu.gyro, old_g))
     old_g = mpu.gyro
     return res
+
+
 
 
 if __name__ == "__main__":
@@ -37,10 +41,23 @@ if __name__ == "__main__":
 
     while True:
         try:
-            print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-            print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
-            print("Temperature: %.2f C" % mpu.temperature)
+            delta_gdelta_a = delta_acceleration()
+            delta_g = delta_gyro()
+            print("Delta Acceleration: %.3f, %.3f, %.3f" %  delta_a)
+            print("        Delta Gyro: %.3f, %.3f, %.3f" %  delta_g)
             print("")
+            max_a[0] = max(delta_a[0], max_a[0])
+            max_a[1] = max(delta_a[1], max_a[1])
+            max_a[2] = max(delta_a[2], max_a[2])
+            print("  MAX Acceleration: %.3f, %.3f, %.3f" % max_a)
+            max_g[0] = max(delta_g[0], max_g[0])
+            max_g[1] = max(delta_g[1], max_g[1])
+            max_g[2] = max(delta_g[2], max_g[2])
+            print("          MAX Gyro: %.3f, %.3f, %.3f" % max_g)
+            #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+            #print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+            #print("Temperature: %.2f C" % mpu.temperature)
+            #print("")
         except:
             print("error")
 

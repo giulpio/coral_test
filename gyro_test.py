35edab0be458460e20501b89fd2c2a115d2ecfe0
giulpio <giulpiomilani@gmail.com>
2022-11-02 12:47:26 +0100

    log gyro delta functions

---
+++ gyro_test.py
@@ -0,0 +1,22 @@
+from gyro import delta_gyro as dg
+from led import led
+from random import randint
+import time
+
+if __name__ == "__main__":
+    try:
+        while True:
+            try:
+                delta = dg()
+                if dg[0] > 0.2 or dg[1] > 0.2 or dg[2] > 0.2:
+                    led(randint(0,255), randint(0,255), randint(0,255))
+                    time.sleep(0.2)
+                    dg()
+                    print("move")
+                else:
+                    pass
+            except:
+                print("some errors")
+
+    except KeyboardInterrupt:
+        print("end")

ba92d0ad191837c83e850f9eb1512ea873c636bb
giulpio <giulpiomilani@gmail.com>
2023-03-10 17:31:55 +0100

    add shutdown led

--- shutdown.py
+++ shutdown.py
@@ -1,6 +1,9 @@
 import board
 import busio
 import time
+import library.led as led
+
+
 
 import os
 
@@ -10,9 +13,11 @@ from playsound import playsound
 #play shutdown sound sync
 
 def shutdown():
+    led.led(255, 50, 0)
     playsound('./static/sounds/beep-01a.mp3', block=True)
     print("power cable unplugged --> closing program and shutdown system")
+    led.led(0,0,0)
     os.system("sudo shutdown")
     
 if __name__ == "__main__":
-	shutdown()
+    shutdown()

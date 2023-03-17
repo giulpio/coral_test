82093750ca5f4987f859338f2648fa35d639092b
giulpio <giulpiomilani@gmail.com>
2023-03-17 14:45:45 +0100

    h

--- old_coral_test.py
+++ old_coral_test.py
@@ -74,41 +74,4 @@ except KeyboardInterrupt:
 except Exception as e:
     print(type(e).__name__ + ': ' + str(e))
 
-"""
-
-state = 0
-maxstate = 2
-
-
-
-try:
-    while True:
-        if read(switch):
-            print("read_switch")
-            #time.sleep(1)
-            #state+=1
-            #if state > maxstate:
-            #    state = 0
-            led(0,0,255)
-            print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.mpu.acceleration))
-        if state == 0:
-            if read(button1):
-                led(255,0,0)
-            elif read(button2):
-                led(0,255,0)
-            else:
-                led(255,255,255)
-        elif state == 1:
-            led(0,0,255)
-        else:
-            led(0,0,0)
-        
-        if gyro.mpu.acceleration[1] > 2 or gyro.mpu.acceleration[1] < -1:
-            led(255,255,0)
-        #time.sleep(0.2)
-
-
-except KeyboardInterrupt:
-    deinit()
-    print("error")
-#shutdown.shutdown()"""
+    
